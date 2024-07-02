fa15516591d0264729d6d4ea6716ec0f6237e556
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-07-02 17:35:38 -0300

    refac: returning Onboarding page on build

--- lib/neopeso_app.dart
+++ lib/neopeso_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:neopeso/features/onboarding/onboarding.dart';
 
 class NeopesoApp extends StatelessWidget {
   const NeopesoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Onboarding(),
+    );
   }
 }
