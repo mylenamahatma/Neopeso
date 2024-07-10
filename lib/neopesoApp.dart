2f0cd53525a78eb0854462c311ee23e9950e4af6
Beatriz Leite dos Santos <123493745+beatrizldsantos@users.noreply.github.com>
2024-07-09 22:26:17 -0300

    Telas de Cadastro das Pacientes

--- lib/neopesoApp.dart
+++ lib/neopesoApp.dart
@@ -1,8 +1,8 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'features/cadastro_paciente_dados_pessoais.dart';
-import 'features/cadastro_paciente_gestacao.dart';
-import 'features/cadastro_paciente_informacoes_partos.dart';
+import 'features/patient/cadastro_paciente_dados_pessoais.dart';
+import 'features/patient/cadastro_paciente_gestacao.dart';
+import 'features/patient/cadastro_paciente_informacoes_partos.dart';
 
 
 void main() {
